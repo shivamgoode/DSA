8304b7ad19d5b9b5c7251e0b58f01c5752ee11c2
Shivam kumar <shivam68338@gmail.com>
2026-08-15 00:57:54 +0530

    Update 3090-maximum-length-substring-with-two-occurrences.cpp

--- 3090-maximum-length-substring-with-two-occurrences/3090-maximum-length-substring-with-two-occurrences.cpp
+++ 3090-maximum-length-substring-with-two-occurrences/3090-maximum-length-substring-with-two-occurrences.cpp
@@ -1,34 +1,21 @@
 class Solution {
 public:
     int maximumLengthSubstring(string s) {
-        vector<char> arr;
-        int ans = 0;
-
-        for (int i = 0; i < s.length(); i++) {
-            int count = 0;
-
-            
-            for (int j = 0; j < arr.size(); j++) {
-                if (arr[j] == s[i]) {
-                    count++;
+        int n=s.length();
+        int left=0;
+        int right=0;
+        int ans=0;
+        unordered_map<char,int> freq;
+            for(right=0;right<n;right++)
+            {
+                  freq[s[right]]++;
+                while(freq[s[right]]>2)
+                {
+                    freq[s[left]]--;
+                    left++;
                 }
+                 ans=max(ans,right-left+1);
             }
-
-           
-            if (count == 2) {
-                for (int j = 0; j < arr.size(); j++) {
-                    if (arr[j] == s[i]) {
-                        arr.erase(arr.begin(), arr.begin() + j + 1);
-                        break;
-                    }
-                }
-            }
-
-            arr.push_back(s[i]);
-
-            ans = max(ans, (int)arr.size());
-        }
-
-        return ans;
+            return ans;
     }
 };
